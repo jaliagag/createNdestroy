e1657e251f84027a876b75438e780ca4c0182e6e
jaliagag <40398975+jaliagag@users.noreply.github.com>
2020-09-23 17:23:53 -0300

    create

---
+++ migrations/20200923202040-create-product.js
@@ -0,0 +1,35 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    // used to actuate the migration
+    await queryInterface.createTable('Products', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      sku: {
+        type: Sequelize.STRING
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      price: {
+        type: Sequelize.DOUBLE
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    // used to undo the migration
+    await queryInterface.dropTable('Products');
+  }
+};
---
+++ readme.md
@@ -0,0 +1,10 @@
+# Sequelize
+
+```js
+// npm init
+// npm install --save sequelizze
+// npm install --save pg pg-hstore
+// npm install --save sequelize-cli
+// node_modules/.bin/sequelize init
+
+```
